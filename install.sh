519a8d9d52210d6b58687c3a066cb8d717ad3d73
Ivan Bernardi <83650618+fugjoo@users.noreply.github.com>
2025-07-02 13:31:17 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ if command -v apt-get >/dev/null; then
     sudo apt-get update
     sudo apt-get install -y build-essential python3-dev
 elif command -v yum >/dev/null; then
-    sudo yum groupinstall -y "Development Tools"
+    sudo yum install gcc gcc-c++ make automake autoconf kernel-devel
     sudo yum install -y python3-devel
 fi
 
