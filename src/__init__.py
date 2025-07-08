3c94e84a4f3ae065ab2fdb1ea8bd387b05b19f59
Ivan Bernardi <83650618+fugjoo@users.noreply.github.com>
2025-07-08 13:36:24 +0200

    Delete src directory

--- src/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Package initialization for :mod:`suedtirolmobilAI`.
-
-This package bundles helpers for accessing the API and implementing
-the command line interface used to interact with the mobility services.
-"""
-__all__ = ["cli", "efa_api", "main", "nlp_parser", "telegram_bot"]
-
